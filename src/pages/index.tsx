eeace30d553e9a505d9a70377168577a82e68d0d
Adam Rackis <adamrackis@hotmail.com>
2024-09-01 20:16:08 -0500

    spring with default settings

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,24 @@
 import { motion } from "framer-motion";
+import { useState } from "react";
+
+const WIDTH = "500px";
 
 export default function Home() {
+  const [open, setOpen] = useState(false);
+
   return (
     <main className={`flex flex-col p-24 gap-3`}>
       <div>Hello</div>
-      <button className="border p-2 self-start">Toggle</button>
-      <div className="w-[500px] h-[700px] bg-red-500"></div>
+      <button onClick={() => setOpen((val) => !val)} className="border p-2 self-start">
+        Toggle
+      </button>
+      <motion.div
+        animate={{
+          width: open ? WIDTH : 0,
+        }}
+        transition={{ type: "spring", damping: 10, mass: 1, stiffness: 100 }}
+        className="w-[0] h-[700px] bg-red-500"
+      ></motion.div>
     </main>
   );
 }
